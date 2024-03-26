1b1d67ae7f260aeac14cccc0dd371e3563072e5e
DRSPACEMAN94 <159204742+DRSPACEMAN94@users.noreply.github.com>
2024-03-25 22:51:17 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ app.load('https://prod.spline.design/OrB3ZM-wUzT0afzg/scene.splinecode').then(()
 });
 
 // Establish WebSocket connection outside the load promise
-const socket = new WebSocket('wss://eed5-2603-8000-d300-d1e-4d49-36e0-8617-c66f.ngrok-free.app'); // Replace with your actual WebSocket server URL
+const socket = new WebSocket('wss://9f21-2603-8000-d300-d1e-cc49-bb48-c9e-a482.ngrok-free.app'); // Replace with your actual WebSocket server URL
 
 socket.onopen = function(event) {
     console.log("Connected to WebSocket server");
